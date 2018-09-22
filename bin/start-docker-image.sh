1510d2f3b44a9a7ca19c4376dad8260ac98cde0b
Nathan Miller <nmiller217@gmail.com>
2018-09-22 14:36:08 -0700

    fix ownership by root after docker imafe runs and add example environment.sh example

--- bin/start-docker-image.sh
+++ bin/start-docker-image.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
 docker-compose -f docker-compose.yml up
+
+# fix ownership
+echo "Fixing ownership on Linux"
+if [ `uname -s` = "Linux" ]
+then
+  ls -l
+  echo "sudo chown -R `id -u $USER`:`id -g $USER` ."
+  sudo chown -R `id -u $USER`:`id -g $USER` .
+  ls -l
+fi
