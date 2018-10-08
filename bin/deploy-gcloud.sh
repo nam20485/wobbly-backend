a4eafb591fb9e07d12dcd2c408294fe4efe47a81
nam20485 <nmiller217@gmail.com>
2018-10-08 10:52:27 -0700

    try alternative gcloud sdk install

--- bin/deploy-gcloud.sh
+++ bin/deploy-gcloud.sh
@@ -6,20 +6,20 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     # Push only if we're testing the master branch
     #if [ "$TRAVIS_BRANCH" == "master" ]; then
 
-        export GAE_PYTHONPATH=${HOME}/.cache/google_appengine 
-        export PATH=$PATH:${HOME}/google-cloud-sdk/bin 
-        export PYTHONPATH=${PYTHONPATH}:${GAE_PYTHONPATH} 
+        #export GAE_PYTHONPATH=${HOME}/.cache/google_appengine 
+        #export PATH=$PATH:${HOME}/google-cloud-sdk/bin 
+        #export PYTHONPATH=${PYTHONPATH}:${GAE_PYTHONPATH} 
+        #if [ ! -d "${GAE_PYTHONPATH}" ]; then python scripts/fetch_gae_sdk.py $(dirname "${GAE_PYTHONPATH}"); fi
+        #if [ ! -d ${HOME}/google-cloud-sdk ]; then curl https://sdk.cloud.google.com | bash; fi        
+
         export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 
         openssl aes-256-cbc -K $encrypted_b48b32dc2f5c_key -iv $encrypted_b48b32dc2f5c_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
-        if [ ! -d "${GAE_PYTHONPATH}" ]; then python scripts/fetch_gae_sdk.py $(dirname "${GAE_PYTHONPATH}"); fi
-        if [ ! -d ${HOME}/google-cloud-sdk ]; then curl https://sdk.cloud.google.com | bash; fi
         tar -xzf credentials.tar.gz
         mkdir -p lib
-        gcloud auth activate-service-account --key-file client-secret.json
 
-        gcloud config set project wobbly-backend
-        
+        gcloud auth activate-service-account --key-file client-secret.json
+        gcloud config set project wobbly-backend        
         gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin $DOCKER_REPO
 
         #cat client-secret.json | docker login -u _json_key --password-stdin $DOCKER_REPO
