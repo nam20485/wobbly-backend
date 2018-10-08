55d9d4661205b9b77200dad4dcefb32eedd98b24
nam20485 <nmiller217@gmail.com>
2018-10-08 12:22:44 -0700

    remove sudo commands

--- bin/install-gcloud-sdk.sh
+++ bin/install-gcloud-sdk.sh
@@ -2,8 +2,8 @@
 
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
-echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-sudo apt-get update -qqy
-sudo apt-get install -qqy google-cloud-sdk=219.0.1-0
+apt-get update -qqy
+apt-get install -qqy google-cloud-sdk=219.0.1-0
