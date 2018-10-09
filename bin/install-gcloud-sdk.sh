ea2e1ff5cffce41f1003d712e14d6b8d6bf72939
Nathan Miller <nmiller217@gmail.com>
2018-10-08 19:24:41 -0700

    hide progress bar for curl

--- bin/install-gcloud-sdk.sh
+++ bin/install-gcloud-sdk.sh
@@ -3,7 +3,7 @@
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 
 echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl -s -S https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo apt-get update -qqy
 sudo apt-get install -qqy google-cloud-sdk=219.0.1-0
