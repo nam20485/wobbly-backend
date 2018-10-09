cce1f777eec1790bc213f0ec7008b66566bc63ac
Nathan Miller <nmiller217@gmail.com>
2018-10-08 21:20:05 -0700

    remove pegged versions

--- bin/install-gcloud-sdk.sh
+++ bin/install-gcloud-sdk.sh
@@ -9,7 +9,7 @@ export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 #curl -s -S https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo apt-get update -qqy
-sudo apt-get install -qqy --no-install-recommends google-cloud-sdk=219.0.1-0
+sudo apt-get install -qqy --no-install-recommends google-cloud-sdk #=219.0.1-0
 
 #export GAE_PYTHONPATH=${HOME}/.cache/google_appengine 
 #export PATH=$PATH:${HOME}/google-cloud-sdk/bin 
--- bin/install-kubectl.sh
+++ bin/install-kubectl.sh
@@ -9,4 +9,4 @@ export KUBECTL_REPO="kubernetes-$(lsb_release -c -s)"
 #echo "deb https://apt.kubernetes.io/ $KUBECTL_REPO main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update -qqy
-sudo apt-get install -qqy --no-install-recommends kubectl=1.12.0-00
+sudo apt-get install -qqy --no-install-recommends kubectl #=1.12.0-00
