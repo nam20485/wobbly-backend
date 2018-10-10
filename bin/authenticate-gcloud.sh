eef6f0a5ff3437561df1945fe13261824fc0eea9
Nathan Miller <nmiller217@gmail.com>
2018-10-10 16:21:11 -0700

    increase verbosity level for kubectl and gcloud

--- bin/authenticate-gcloud.sh
+++ bin/authenticate-gcloud.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#export COMPUTE_ZONE="us-west2-a"
+export COMPUTE_ZONE="us-west2-a"
 export GCLOUD_PROJECT="wobbly-backend"
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
@@ -9,7 +9,7 @@ openssl aes-256-cbc -K $encrypted_b48b32dc2f5c_key -iv $encrypted_b48b32dc2f5c_i
 tar -xzf credentials.tar.gz
 mkdir -p lib
 
-gcloud auth activate-service-account --key-file client-secret.json
+gcloud auth activate-service-account --key-file client-secret.json --verbosity="info"
 #gcloud config set project $GCLOUD_PROJECT
-gcloud config set compute/zone $COMPUTE_ZONE    
-gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin $DOCKER_REPO
+gcloud config set compute/zone $COMPUTE_ZONE --verbosity="info"
+gcloud auth print-access-token --verbosity="info" | docker login -u oauth2accesstoken --password-stdin $DOCKER_REPO
--- bin/provision.sh
+++ bin/provision.sh
@@ -2,21 +2,21 @@
 
 export CLUSTER_NAME="wobbly-backend-cluster"
 export DEPLOYMENT_NAME="wobbly-backend-app"
-export COMPUTE_ZONE="us-west2-b"
+export COMPUTE_ZONE="us-west2-a"
 
-gcloud config set compute/zone $COMPUTE_ZONE    
+gcloud config set compute/zone $COMPUTE_ZONE --verbosity="info"
 
 # create kubernetes cluster
 #gcloud container clusters create $CLUSTER_NAME
 
 # authenticate to new cluster
-gcloud container clusters get-credentials $CLUSTER_NAME
+gcloud container clusters get-credentials $CLUSTER_NAME --verbosity="info"
 
 # deploy image to cluster
 #kubectl run $DEPLOYMENT_NAME --image $REMOTE_DOCKER_PATH --port 8000
 
 # update cluster's deployed image
-kubectl set image deployment/"$DEPLOYMENT_NAME" "$DEPLOYMENT_NAME"="$REMOTE_DOCKER_PATH":latest
+kubectl set image deployment/"$DEPLOYMENT_NAME" "$DEPLOYMENT_NAME"="$REMOTE_DOCKER_PATH":latest --v=4
 
 # expose service
-kubectl expose deployment $DEPLOYMENT_NAME --type LoadBalancer --port 8000 --target-port 8000
+kubectl expose deployment $DEPLOYMENT_NAME --type LoadBalancer --port 8000 --target-port 8000 --v=4
