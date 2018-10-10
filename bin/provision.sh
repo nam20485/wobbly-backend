27ced9007166f143c9fd31661fed9b7829f70dbf
Nathan Miller <nmiller217@gmail.com>
2018-10-10 00:47:50 -0700

    fix kubectl set image command and re-enable test.sh

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export CLUSTER_NAME="wobbly-backend-cluster"
-export DEPLOYMENT_NAME="wobbly-backend-deployment"
+export DEPLOYMENT_NAME="wobbly-backend-app"
 
 # create kubernetes cluster
 #gcloud container clusters create $CLUSTER_NAME
@@ -12,7 +12,8 @@ gcloud container clusters get-credentials $CLUSTER_NAME
 # deploy image to cluster
 #kubectl run $DEPLOYMENT_NAME --image $REMOTE_DOCKER_PATH --port 8000
 
-kubectl set image "$CLUSTER_NAME"/"$DEPLOYMENT_NAME" "$DEPLOYMENT_NAME"="$REMOTE_DOCKER_PATH":latest
+# update cluster's deployed image
+kubectl set image deployment/"$DEPLOYMENT_NAME" "$DEPLOYMENT_NAME"="$REMOTE_DOCKER_PATH":latest
 
 # expose service
 kubectl expose deployment $DEPLOYMENT_NAME --type LoadBalancer --port 8000 --target-port 8000
