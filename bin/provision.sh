1f8fddec1dc5fe7b76a5a1414829745a6cad811b
Nathan Miller <nmiller217@gmail.com>
2018-10-10 00:01:49 -0700

    enclose env vars in quotes

--- bin/provision.sh
+++ bin/provision.sh
@@ -12,7 +12,7 @@ gcloud container clusters get-credentials $CLUSTER_NAME
 # deploy image to cluster
 #kubectl run $DEPLOYMENT_NAME --image $REMOTE_DOCKER_PATH --port 8000
 
-# expose service
-#kubectl expose deployment $DEPLOYMENT_NAME --type LoadBalancer --port 8000 --target-port 8000
+kubectl set image "$CLUSTER_NAME"/"$DEPLOYMENT_NAME" "$DEPLOYMENT_NAME"="$REMOTE_DOCKER_PATH":latest
 
-kubectl set image $CLUSTER_NAME/$DEPLOYMENT_NAME $DEPLOYMENT_NAME=$REMOTE_DOCKER_PATH:latest
+# expose service
+kubectl expose deployment $DEPLOYMENT_NAME --type LoadBalancer --port 8000 --target-port 8000
