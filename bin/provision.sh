6c6e22ea1ee4487ec7be12e8d09a2efee1e16180
Nathan Miller <nmiller217@gmail.com>
2018-10-09 22:52:51 -0700

    set image instead of creating cluster

--- bin/provision.sh
+++ bin/provision.sh
@@ -4,13 +4,15 @@ export CLUSTER_NAME="wobbly-backend-cluster"
 export DEPLOYMENT_NAME="wobbly-backend-deployment"
 
 # create kubernetes cluster
-gcloud container clusters create $CLUSTER_NAME
+#gcloud container clusters create $CLUSTER_NAME
 
 # authenticate to new cluster
 gcloud container clusters get-credentials $CLUSTER_NAME
 
 # deploy image to cluster
-kubectl run $DEPLOYMENT_NAME --image $REMOTE_DOCKER_PATH --port 8000
+#kubectl run $DEPLOYMENT_NAME --image $REMOTE_DOCKER_PATH --port 8000
 
 # expose service
-kubectl expose deployment $DEPLOYMENT_NAME --type LoadBalancer --port 8000 --target-port 8000
+#kubectl expose deployment $DEPLOYMENT_NAME --type LoadBalancer --port 8000 --target-port 8000
+
+kubectl set image $CLUSTER_NAME/$DEPLOYMENT_NAME $DEPLOYMENT_NAME=$REMOTE_DOCKER_PATH:latest
