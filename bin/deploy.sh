9801d7e903353b884b9fcf472c63dc2761afc38d
Nathan Miller <nmiller217@gmail.com>
2018-11-03 16:21:51 -0700

    test live site

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -17,11 +17,15 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         export IMAGE_NAME="$DOCKER_IMAGE"-"$TRAVIS_BRANCH"
         export CLUSTER_NAME="backend-cluster"-"$TRAVIS_BRANCH"
         export DEPLOYMENT_NAME="backend-app"-"$TRAVIS_BRANCH"
+        export SERVICE_NAME="backend-app-service"-"$TRAVIS_BRANCH"
 
         source bin/install-tools.sh
         source bin/authenticate-gcloud.sh
         source bin/push-docker.sh
         source bin/deploy-kube.sh
+
+        kubectl get service $SERVICE_NAME -o json 
+        export ENDPOINT="http://35.236.98.243:8000/"
 
    else
        echo "Skipping deploy because branch is not staging or production"
