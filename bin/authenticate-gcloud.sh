5bc797893177c79b78686166739350c80fb86641
Nathan Miller <nmiller217@gmail.com>
2018-11-03 15:59:03 -0700

    fix tar command

--- bin/authenticate-gcloud.sh
+++ bin/authenticate-gcloud.sh
@@ -6,7 +6,7 @@ openssl aes-256-cbc -K $encrypted_b48b32dc2f5c_key -iv $encrypted_b48b32dc2f5c_i
 tar -xzf credentials/credentials.tar.gz --directory credentials/
 mkdir -p lib
 
-gcloud auth activate-service-account --key-file client-secret.json
+gcloud auth activate-service-account --key-file credentials/client-secret.json
 gcloud config set project $GCLOUD_PROJECT
 gcloud config set compute/zone $COMPUTE_ZONE
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin $DOCKER_REPO
