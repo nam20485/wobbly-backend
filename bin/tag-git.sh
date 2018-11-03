4c357146182f7efebe0c5c95024bc66fce675204
Nathan Miller <nmiller217@gmail.com>
2018-11-03 15:13:42 -0700

    fix tagging

--- bin/tag-git.sh
+++ bin/tag-git.sh
@@ -2,5 +2,5 @@
 
 GIT_TAG="$TRAVIS_BRANCH"-"$TRAVIS_BUILD_NUMBER"
 
-git tag -a -m "travis build tag"-"$GIT_TAG"
-#git push --tags
+git tag -a "$GIT_TAG" -m "travis build tag-$GIT_TAG"
+git push --tags
