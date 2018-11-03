f9450f6e67af6c4c1fd1055121453963157e622a
Nathan Miller <nmiller217@gmail.com>
2018-11-03 14:48:23 -0700

    add tag.shand move credentials to own folder

--- bin/authenticate-gcloud.sh
+++ bin/authenticate-gcloud.sh
@@ -3,7 +3,7 @@
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 
 openssl aes-256-cbc -K $encrypted_b48b32dc2f5c_key -iv $encrypted_b48b32dc2f5c_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
-tar -xzf credentials.tar.gz
+tar -xzf credentials/credentials.tar.gz
 mkdir -p lib
 
 gcloud auth activate-service-account --key-file client-secret.json
